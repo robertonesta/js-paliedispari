421b28532b52bb84135845e36a88cc9c6cf7fbf1
RobertoNesta <robertonesta1993@gmail.com>
2023-03-16 16:20:27 +0100

    adding the even/odds user choice function

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,15 +20,29 @@ if (userWord == reverseUserWord) {
 }
 const userWord = prompt("type a word to see if it's a palindrome")
 const value = Palindrome(userWord) //call the function
-console.log("value")
 
 
 /*Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.*/
 
 // far scegliere pari o dispari all'utente
+
+let evenOdds = prompt("choose between even and odds")
+console.log(evenOdds)
+
+function even_odds (){
+    if (evenOdds == 'even'){ 
+        console.log(`even`)
+    } else if (evenOdds == 'odds'){
+        console.log(`odds`)
+    } else {
+        console.log(`you cane only choose between even and odds`)
+    }
+}
+
+
 // far scegliere all'utente un numero da 1 a 5
-// generare un numero random al pc
+// generare un numero random per il pc
 // sommare il numero dell'utente e il numero del pc
 // se la somma è uguale al pari o dispari scelto dall'utente, quest'ultimo ha vinto, altrimenti ha vinto il computer
 
