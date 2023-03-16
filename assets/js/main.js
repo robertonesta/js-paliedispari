23fe92e7a63ee834c7788080e6f03770cb9ffad8
RobertoNesta <robertonesta1993@gmail.com>
2023-03-16 16:03:42 +0100

    split the second exercise into small requests

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,22 @@ if (userWord == reverseUserWord) {
     alert(`not a palindrome`)
 }
 }
-
 const userWord = prompt("type a word to see if it's a palindrome")
-
 const value = Palindrome(userWord) //call the function
-
 console.log("value")
+
+
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.*/
+
+// far scegliere pari o dispari all'utente
+// far scegliere all'utente un numero da 1 a 5
+// generare un numero random al pc
+// sommare il numero dell'utente e il numero del pc
+// se la somma è uguale al pari o dispari scelto dall'utente, quest'ultimo ha vinto, altrimenti ha vinto il computer
+
+
+
 
 
 
